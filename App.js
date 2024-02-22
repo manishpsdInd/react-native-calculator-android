69abc0abea96129caf2f5b9cc320def24630aeb0
manishpsdind <manishpsd@gmail.com>
2024-02-22 15:00:59 +0530

    3rd Commit, working example

--- App.js
+++ App.js
@@ -1,12 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default function App() {
+import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+
+export default App = () => {
+
+  const symbols = ["+", "-", "*", "/", "%"]
+
+  const [operator, setOperator] = React.useState('');
+  const [operand1, setOperand1] = React.useState('');
+  const [operand2, setOperand2] = React.useState('');
+  const [result, setResult] = React.useState('');
+
+  const callCalculate = () => {
+    let calculatedResult=0
+    switch (operator) {
+      case "+":
+        calculatedResult = Number(operand1) + Number(operand2) 
+        break
+      case "-":
+        calculatedResult = Number(operand1) - Number(operand2)
+        break
+      case "*":
+        calculatedResult = Number(operand1) * Number(operand2)
+        break
+      case "/":
+        calculatedResult = Number(operand1) / Number(operand2)
+        break
+      case "%":
+        calculatedResult = Number(operand1) % Number(operand2)
+        break    
+      default:
+        calculatedResult = Number(operand1) + Number(operand2)
+    }  
+    console.log("num1: "+operand1+" ,num2: "+operand2+" ,operator: "+operator+" , result: "+calculatedResult)
+    setResult(calculatedResult.toString());
+  };
+
+  const handleInputChange1 = (text) => {
+    const numValue = parseInt(text, 10);
+    if (numValue >= 1 && numValue <= 9) {
+      setOperand1(text);
+    } else {
+      setOperand1(text.replace(/^0+(?=\d)/, ''));
+    }
+  };
+
+  const handleInputChange2 = (text) => {
+    const numValue = parseInt(text, 10);
+    if (numValue >= 1 && numValue <= 9) {
+      setOperand2(text);
+    } else {
+      setOperand2(text.replace(/^0+(?=\d)/, ''));
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Hello World</Text>
+    <View style={[styles.container, { padding: 20 }]}>
+      <Text style={styles.textStyle}>Calculator App</Text>
+      <View style={styles.verticalSpace} />
+      <TextInput style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={handleInputChange1} // not required in case of keyboardType="numeric"
+                value={operand1}
+                textAlign={'center'}
+                maxLength={2}
+                //keyboardType="numeric"
+                placeholder="Enter first number"
+                placeholderTextColor="#999"
+            />
+      <View style={styles.verticalSpace} />      
+      <TextInput style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={handleInputChange2} // not required in case of keyboardType="numeric"
+                value={operand2}
+                textAlign={'center'}
+                maxLength={2}
+                //keyboardType="numeric"
+                placeholder="Enter second number"
+                placeholderTextColor="#999"
+            />
+      <View style={styles.verticalSpace} />      
+      <SelectDropdown
+        data={symbols}
+        defaultButtonText={'Select operator'}
+        onSelect={(selectedItem, index) => setOperator(selectedItem)}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          return selectedItem
+        }}
+        rowTextForSelection={(item, index) => {
+          return item
+        }}
+      />
+      <View style={styles.verticalSpace} />
+      <Button title="Calculate" onPress={() => callCalculate()}/>
+      <View style={styles.verticalSpace} />
+      <TextInput style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1}} editable={false} textAlign={'center'}>
+        Output: {result}</TextInput>
     </View>
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,5 +110,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     textAlign: 'center'
-  }
+  },
+  verticalSpace: {
+    height: 20
+  },
 });
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # react-native-calculator-android
+
+## Basic react native example for simple calculator (Android)
+
+## Use below commands to setup and run the project
+`$ npm install -g react-native-cli`
+
+`$ npm install -g create-react-native-app`
+
+`$ create-react-app YOURAPPNAME`
+
+`$ cd RELOCATE-TO-YOURAPPNAME`
+
+`$ react-native run-android`
+
+## Sample output
+
+## Sample 1
+![alt text](Images/2.png)
+
+## Sample 2
+![alt text](Images/1.png)
